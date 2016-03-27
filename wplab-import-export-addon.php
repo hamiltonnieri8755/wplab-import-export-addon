21c289dae03fa45934fc2a704931d3b1f117b170
hamiltonnieri8755 <hamiltonnieri8755@yahoo.com>
2016-03-27 18:58:35 +0300

    init

---
+++ wplab-import-export-addon.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin Name: WPLab Import/Export Plugin
+ * Plugin URI: https://www.wplab.com/
+ * Description: WPLab tool that helps you import/export ...
+ * Version: 1.0
+ * Author: Hamilton Nieri
+ * Author URI: https://www.wplab.com/
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ----------------------------------------------------------------------
+ * Copyright (C) 2016  Hamilton Nieri  (Email: hamiltonnieri8755@yahoo.com)
+ * ----------------------------------------------------------------------
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ----------------------------------------------------------------------
+ */
+
+// Including WP core file
+if ( ! function_exists( 'get_plugins' ) )
+    require_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+// Whether plugin active or not
+if ( is_plugin_active( 'woocommerce/woocommerce.php' ) ) :
+
+	
+
+endif;
